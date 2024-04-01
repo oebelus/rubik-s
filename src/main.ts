132f3f57e3cec31e0fbcfa8b9296b69221a4149a
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2024-04-01 23:06:00 +0000

    rotation

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,8 @@ controls.enableZoom = false;
 
 const geometry = new THREE.BoxGeometry(1, 1, 1).toNonIndexed()
 const material = new THREE.MeshBasicMaterial({
-  vertexColors: true
+  vertexColors: true,
+  side: THREE.DoubleSide
 })
 
 const positionAttribute = geometry.getAttribute("position")
@@ -47,14 +48,17 @@ geometry.setAttribute('color', new THREE.Float32BufferAttribute(threeColors, 3))
 const cubes: THREE.Mesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.MeshBasicMaterial, THREE.Object3DEventMap>[] = []
 
 const cubesNumber = 3
-for (let i = 0; i < cubesNumber; i++) {
-  for (let j = 0; j < cubesNumber; j++) {
-    for (let k = 0; k < cubesNumber; k++) {
-      const edges = new THREE.EdgesGeometry(geometry); 
-      const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({color: 0x000000, linewidth: 10})); 
+const start = -cubesNumber % 2
+for (let i = start; i < cubesNumber-1; i+=1) {
+  for (let j = start; j < cubesNumber-1; j+=1) {
+    for (let k = start; k < cubesNumber-1; k+=1) {
+      //const edges = new THREE.EdgesGeometry(geometry); 
+      //const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({color: 0x000000, linewidth: 10})); 
       const cube = new THREE.Mesh(geometry, material)
-      line.material.linewidth = 10
-      cube.add(line)
+      /*line.material.linewidth = 10
+      line.add(cube)*/
+      cube.userData.draggable = true
+      cube.userData.name = `Cube: ${i}, ${j}, ${k}`
       cube.position.set(i, j, k)
       cubes.push(cube)
     }
@@ -64,15 +68,99 @@ for (let i = 0; i < cubesNumber; i++) {
 for (var cube of cubes)
   scene.add(cube)
 
+const raycaster = new THREE.Raycaster()
+
+let initialClick = new THREE.Vector2()
+let dragged = new THREE.Vector2()
+
+var draggable: THREE.Object3D
+
+window.addEventListener('click', event => {
+  /*if (draggable) {
+    console.log(`Dropping draggable: ${draggable.userData.name}`)
+    draggable = null as any
+    return 
+  }*/
+
+  var mouseVector = new THREE.Vector2(
+    event.clientX / window.innerWidth * 2 - 1,
+    -event.clientY / window.innerHeight * 2 + 1
+  );
+
+  initialClick = mouseVector.copy(mouseVector);
+  console.log("initial click", initialClick);
+
+  raycaster.setFromCamera(mouseVector, camera)
+  const intersects = raycaster.intersectObjects(scene.children)
+
+  if (intersects.length > 0 && intersects[0].object.userData.draggable) {
+    console.log(intersects[0].object.position)
+    draggable = intersects[0].object
+  }
+})
+
+let mouseVector = new THREE.Vector2()
+
+window.addEventListener('mousemove', event => {
+  mouseVector = new THREE.Vector2(
+    event.clientX / window.innerWidth * 2 - 1,
+    -event.clientY / window.innerHeight * 2 + 1
+  );
+  dragged = mouseVector.copy(mouseVector);
+  console.log("dragged", dragged);
+})
+
+/*const xAxis = new THREE.Vector3(1, 0, 0)
+const yAxis = new THREE.Vector3(0, 0, 1)*/
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
 const quaternion = new THREE.Quaternion();
+const degree = Math.PI / 2
+
+function dragObject() {
+  if (draggable != null) {
+    raycaster.setFromCamera(mouseVector, camera)
+    const intersect = raycaster.intersectObjects(scene.children)
+    if (intersect.length > 0) {
+      for (let sect of intersect) {
+        if (!sect.object.userData.draggable) {
+          continue
+        }
+
+        let target = sect.point
+
+        const toRotate: THREE.Mesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.MeshBasicMaterial, THREE.Object3DEventMap>[] = []
+        for (var cube of cubes) {
+          if (cube.position.x == draggable.position.x)
+            toRotate.push(cube)
+        }
+
+        const group = new THREE.Group()
+        for (var cube of toRotate) {
+          group.add(cube)
+        }
+        scene.add(group)
+        if (initialClick.x - dragged.x < 0)
+          group.rotation.x = -target.x
+        else 
+          group.rotation.x = target.x
+      }
+    }
+  }
+}
+
+
 const axis = new THREE.Vector3(0, 1, 1)
-const degree = 45
+
 quaternion.setFromAxisAngle(axis.normalize(), THREE.MathUtils.degToRad(degree));
 
 camera.position.z = 7.5;
 
 function animate() {
   requestAnimationFrame(animate);
+  dragObject()
 	renderer.render(scene, camera);
 }
 animate();
